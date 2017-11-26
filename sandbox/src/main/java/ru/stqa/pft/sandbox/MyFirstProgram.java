76f3f1bfb200ba7db3dd7f476e6d542de1888f29
VitalikZ <vitaliy.zakorchevniy@gmail.com>
2017-11-26 21:03:26 +0200

    Fail attempt

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,28 +3,25 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("world");
+    /*hello("world");
     hello("User");
     hello("Vitalii");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.p1 + " и " + r.p2 + " = " + r.area());
+*/
+    Point s = new Point(5, 5);
+  }
+
+  public static double distance(Point p1, Point p2){
 
   }
 
-  public static void hello(String somebody) {
+
+  /*public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
+  }*/
 }
