fc6da615fe7045bb88b665209c0b7a736ae3d4e0
Vitalii Zak <vitaliy.zakorchevniy@gmail.com>
2017-11-29 11:59:44 +0200

    Point tests

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,7 +7,13 @@ public class MyFirstProgram {
     Point point1 = new Point(0, 0);
     Point point2 = new Point(3, 4);
 
-    System.out.println("Дистанция между точками = " + point1.distance(point2));
+    String textDistance = "Дистанция между точками = ";
+
+    System.out.println(textDistance + point1.distance(point2));
+
+
+
+
 
   }
 }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,4 +1,29 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class PointTests {
+
+
+  @Test
+  public void testPointDistanceIsWright() {
+    Point point1 = new Point(0, 0);
+    Point point2 = new Point(3, 4);
+    Assert.assertEquals(point1.distance(point2), 5.0);
+  }
+
+  @Test
+  public void testPointDistanceNotInt() {
+    Point point1 = new Point(0, 0);
+    Point point2 = new Point(3, 4);
+    Assert.assertNotEquals(point1.distance(point2), 5);
+  }
+
+  @Test
+  public void testPointDistanceNotString() {
+    Point point1 = new Point(0, 0);
+    Point point2 = new Point(3, 4);
+    Assert.assertNotEquals(point1.distance(point2), "5");
+  }
 }
