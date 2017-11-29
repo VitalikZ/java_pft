c55ddfc2e8cc4c2e6f109bfd1720f2358ef06773
Vitalii Zak <vitalii.zakorchevnyy@gmail.com>
2017-11-29 08:42:17 +0200

    class MyFirstProgram without comments code

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,56 +7,8 @@ public class MyFirstProgram {
     Point point1 = new Point(0, 0);
     Point point2 = new Point(3, 4);
 
-
     System.out.println("Дистанция между точками = " + point1.distance(point2));
 
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-    Point point1 = new Point(0, 0);
-    Point point2 = new Point(3, 4);*/
-
-    /*hello("world");
-    hello("User");
-    hello("Vitalii");
-
-    Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-
-    Point r = new Point(4, 6);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-
-        *//*Point point1 = new Point(0, 0);
-        Point point2 = new Point(3, 4);
-        System.out.println("Расстояние между точкой point1 " + point1.p1 + " и point2 " + point1.p2 + " = " + point1.distance());
-        System.out.println("Расстояние между точкой point1 " + point2.p1 + " и point2 " + point2.p2 + " = " + point1.distance());*//*
-
-
-    public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody + "!");
-    }*/
