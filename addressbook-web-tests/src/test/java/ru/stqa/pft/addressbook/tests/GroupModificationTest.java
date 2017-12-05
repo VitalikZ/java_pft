7da735cea0da8802a48034ecb0f37c885f40146e
Vitalii Zak <vitaliy.zakorchevniy@gmail.com>
2017-12-05 15:27:22 +0200

    create common base class - HelperBase for classes helper

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -0,0 +1,15 @@
+/*
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupModificationTest extends TestBase{
+
+  @Test
+  public void testGroupModofication(){
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    //app.getGroupHelper().initGroupModofication();
+  }
+}
+*/
