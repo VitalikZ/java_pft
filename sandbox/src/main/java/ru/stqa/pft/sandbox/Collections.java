98a4f2af8dcd9b3e875b78c188121fc24c62c8fd
VitayiZ <vitaliy.zakorchevniy@gmail.com>
2017-12-22 16:21:00 +0200

    create Collections(l4.3) in sandbox

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,14 +1,25 @@
-/*
 package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Collections {
 
   public static void main(String[] args) {
-    String[] langs = new String[4];
-    langs[0] = "Java";
-    langs[1] = "C#";
-    langs[2] = "Python";
-    langs[3] = "PHP";
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    /*List<String> languages = new ArrayList<String>();
+    languages.add("Java");
+    languages.add("C#");
+    languages.add("Python");
+    */
+
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+
+    for(String l : languages){
+      System.out.println("Я хочу выучить " + l);
+    }
   }
 }
-*/
