058f7b4ce14190f98f7ebd08659057fec4f18189
VitayiZ <vitaliy.zakorchevniy@gmail.com>
2018-01-11 16:10:55 +0200

    try to fix getContactList

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -109,8 +109,8 @@ public class ContactHelper extends HelperBase {
     List<ContactData> contacts = new ArrayList<ContactData>();
     List<WebElement> elements = wd.findElements(By.xpath("//table[@id='maintable']/tr[@name='entry']"));
     for (WebElement element : elements){
-      String name_test = element.getText();
-      String lastname_test = element.getText();
+      String name_test = element.findElement(By.xpath(".//td[3]")).getText();
+      String lastname_test = element.findElement(By.xpath(".//td[2]")).getText();
       ContactData contact = new ContactData(name_test, lastname_test, null, null, null, null);
       contacts.add(contact);
     }
