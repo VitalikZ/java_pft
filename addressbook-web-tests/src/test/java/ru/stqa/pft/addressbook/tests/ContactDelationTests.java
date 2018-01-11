bfd5e61d2f7e342078170356e57d191a194d3521
VitalikZ <vitaliy.zakorchevniy@gmail.com>
2018-01-11 21:13:46 +0200

    Lecture 4.6 compare all lists, before and after deletion(with genereting method Code-equals() and hashCode())

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelationTests.java
@@ -6,21 +6,24 @@ import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.List;
 
-public class ContactDelationTests extends TestBase{
+public class ContactDelationTests extends TestBase {
 
   @Test
-  public void testDeleteContact(){
+  public void testDeleteContact() {
     app.getContactHelper().goToContactListPage();
-    if(! app.getContactHelper().isThereAContact()){
-      app.getContactHelper().createContact(new ContactData("name_test", "lastname_test", "address_test", "+380731234567", "email-one.test@test.com", "test1"));
+    if (!app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData("name_test", "lastname_test", null, null, null, null));
     }
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().selectContact(before.size() -1);
+    app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().submitContactDeletion();
     app.getContactHelper().acceptDeletionAlert();
     app.getContactHelper().goToContactListPage();
     List<ContactData> after = app.getContactHelper().getContactList();
-    Assert.assertEquals(after.size(), before.size() -1);
+    Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
 
   }
 }
