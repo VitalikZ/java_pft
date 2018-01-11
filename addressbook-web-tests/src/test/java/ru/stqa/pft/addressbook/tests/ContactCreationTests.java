bd104112fa630fcbae807dc03f5655876242163f
VitalikZ <vitaliy.zakorchevniy@gmail.com>
2018-01-11 22:09:26 +0200

    L4.8 Find max value in collections --- create loop for find max value and assert that collections

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -13,9 +14,21 @@ public class ContactCreationTests extends TestBase {
   public void testCreateContact() {
     app.getContactHelper().goToContactListPage();
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().createContact(new ContactData("name_test", "lastname_test", null, null, null, null));
+    ContactData contact = new ContactData("name_test", "lastname_test", null, null, null, null);
+    app.getContactHelper().createContact(contact);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() +1);
+
+    before.add(contact);
+    int max = 0;
+    for(ContactData g : after){
+      if(g.getId() > max){
+        max = g.getId();
+      }
+    }
+    contact.setId(max);
+    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
+
   }
 
 }
