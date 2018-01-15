30aa73eb9773dce99ab2c6c7a1c7ae01f42f8974
VitayiZ <vitaliy.zakorchevniy@gmail.com>
2018-01-15 18:40:55 +0200

    DZ 10(L 5.6.) Hamcrest: modify assertions ----- create hamcrest checks for Modification/Deletion/Creation contact tests and create Contacts class in models

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.util.HashSet;
 import java.util.List;
@@ -108,8 +109,8 @@ public class ContactHelper extends HelperBase {
     return wd.findElements(By.cssSelector("img[alt='Edit']")).size();
   }
 
-  public Set<ContactData> all() {
-    Set<ContactData> contacts = new HashSet<ContactData>();
+  public Contacts all() {
+    Contacts contacts = new Contacts();
     List<WebElement> elements = wd.findElements(By.name("entry"));
     for (WebElement element : elements) {
       String name_test = element.findElement(By.xpath(".//td[3]")).getText();
