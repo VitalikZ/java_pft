502a503aeb2bea9f0ff219412cfa4e9e7364555a
VitayiZ <vitaliy.zakorchevniy@gmail.com>
2018-02-01 11:34:27 +0200

    DZ 19 make skipIfNotFixed and isIssueOpen methods, add to testGetProject and testGetProjectSecond - skipIfNotFoxed method with bug id

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -16,6 +16,17 @@ public class SoapTests extends TestBase {
 
   @Test
   public void testGetProjects() throws MalformedURLException, ServiceException, RemoteException {
+    skipIfNotFixed(1);
+    Set<Project> projects = app.soap().getProjects();
+    System.out.println(projects.size());
+    for (Project project : projects) {
+      System.out.println(project.getName());
+    }
+  }
+
+  @Test
+  public void testGetProjectsSecond() throws MalformedURLException, ServiceException, RemoteException {
+    skipIfNotFixed(2);
     Set<Project> projects = app.soap().getProjects();
     System.out.println(projects.size());
     for (Project project : projects) {
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,13 +1,18 @@
 package ru.stqa.pft.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
@@ -15,16 +20,20 @@ public class TestBase {
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
-  public void skipIfNotFixed(int issueId) {
+  public void skipIfNotFixed(int issueId) throws MalformedURLException, ServiceException, RemoteException {
     if (isIssueOpen(issueId)) {
       throw new SkipException("Ignored because of issue " + issueId);
     }
   }
 
-  private boolean isIssueOpen(int issueId) {
-    return false;
+  public boolean isIssueOpen(int issueId) throws RemoteException, MalformedURLException, ServiceException {
+    MantisConnectPortType mc = app.soap().getMantisConnect();
+    String status = mc.mc_issue_get(app.getProperty("soap.adminLogin"), app.getProperty("soap.adminPass"), BigInteger.valueOf(issueId)).getStatus().getName();
+    if (status.equals("resolved")) {
+      return false;
+    }
+    return true;
   }
-
 
   @BeforeSuite
   public void setUp() throws Exception {
