adbab0bd5944693cadaefb78934e19960c379801
VitayiZ <vitaliy.zakorchevniy@gmail.com>
2018-02-02 10:35:48 +0200

    fix testCreateIssue  in request.get| add ?limit1000

--- rest-sample/src/test/java/ru/stqa/tft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/tft/rest/RestTests.java
@@ -28,7 +28,7 @@ public class RestTests {
   }
 
   private Set<Issue> getIssues() throws IOException {
-    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
+    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json?limit=1000"))
             .returnContent().asString();
     JsonElement parsed = new JsonParser().parse(json);
     JsonElement issues = parsed.getAsJsonObject().get("issues");
